78d4c0b84eef1f26ec7bc5456027365c23db6007
Alex Popa <alexpopa7473@gmail.com>
2026-03-17 11:23:21 +0100

    KMeans Clase
    Añadido la clase KMeans

---
+++ src/main/java/es/uji/al405104/algoritmos/KMeans.java
@@ -0,0 +1,18 @@
+package es.uji.al405104.algoritmos;
+
+import es.uji.al405104.table.Table;
+import java.util.List;
+
+public class KMeans {
+
+    public KMeans(int numClusters, int numIterations, long seed) {
+        // Almacenar número de grupos, iteraciones y semilla.
+    }
+    public void train(Table data) {
+        // Implementación del algoritmo de entrenamiento
+    }
+    public Integer estimate(List<Double> sample) {
+        // Implementación del algoritmo de estimación.
+        return 0;
+    }
+}
--- src/main/java/es/uji/al405104/algoritmos/KNN.java
+++ src/main/java/es/uji/al405104/algoritmos/KNN.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 
 public class KNN {
-  
+
   /// Atributos ///
   private TableWithLabels trainingData;
 
