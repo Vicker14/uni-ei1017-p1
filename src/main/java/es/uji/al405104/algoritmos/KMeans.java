cf2460fd2cda78b235458a740048b126f7bd39cc
Alex Popa <alexpopa7473@gmail.com>
2026-03-17 12:04:15 +0100

    Añadido atributos KMeans
    Añadido los atributos de la clase KMeans y la clasificacion de la clase

--- src/main/java/es/uji/al405104/algoritmos/KMeans.java
+++ src/main/java/es/uji/al405104/algoritmos/KMeans.java
@@ -1,13 +1,23 @@
 package es.uji.al405104.algoritmos;
 
+import es.uji.al405104.table.Row;
 import es.uji.al405104.table.Table;
 import java.util.List;
 
 public class KMeans {
 
+    /// ATRIBUTOS ///
+    int numCLuisters; //Numero de grupos (k) que queremos
+    int numITerations; //Cuantas veces vamos a mover los centros para ajustar los grupos
+    long seed; //Numero al azar
+    List<Row> centroids; //Aqui guardaremos los puntos centrales de cada grupo
+
+    /// CONTRUCTOR ///
     public KMeans(int numClusters, int numIterations, long seed) {
         // Almacenar número de grupos, iteraciones y semilla.
     }
+
+    /// METODOS ///
     public void train(Table data) {
         // Implementación del algoritmo de entrenamiento
     }
