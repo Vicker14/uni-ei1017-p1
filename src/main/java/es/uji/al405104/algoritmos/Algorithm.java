5255ce885c6cd85cd48aa69088c59345ac7c64d9
Alex Popa <alexpopa7473@gmail.com>
2026-03-25 14:20:16 +0100

    Añadido comentarios a Algorithm

--- src/main/java/es/uji/al405104/algoritmos/Algorithm.java
+++ src/main/java/es/uji/al405104/algoritmos/Algorithm.java
@@ -3,9 +3,22 @@ package es.uji.al405104.algoritmos;
 import es.uji.al405104.Excepciones.KMeansExceptions;
 import es.uji.al405104.table.Table;
 
-public interface Algorithm<T extends Table, S, R> {
+/*
+    Un algoritmo para que pueda ser un algoritmo necesitamos 2 cosas
+        -El metodo con el cual lo vamos a entrenar
+        -El metodo con el que estimaremos y prediciremos el resultado
 
-    void train(T data) throws KMeansExceptions;
+    Por otro lado para poder cumplir esto a su vez necesitamos 3 datos:
+        -Los datos de entrenamiento (Table o TableWithLabels)
+        -Los datos que le pasamos a estimate, Sample
+        -El resultaod final de los algoritmos, Result
+ */
 
-    R estimate(S sample);
+public interface Algorithm<T extends Table, S, R> { //T: training data, S: sample, R: result
+
+    // Cotruye el momdelo predictivo a partir de un conjutno de datos de entrenamiento
+    public void train(T data) throws KMeansExceptions;
+
+    //Realiza una estimacion o prediccion para una muestra basada en lo aprendido
+    public R estimate(S sample);
 }
