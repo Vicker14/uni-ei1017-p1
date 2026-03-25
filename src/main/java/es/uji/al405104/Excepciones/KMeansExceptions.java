a74a350499acb7f8562172a931b024c4848e0a9e
Alex Popa <alexpopa7473@gmail.com>
2026-03-25 14:31:59 +0100

    Añadido super a KMeansException

--- src/main/java/es/uji/al405104/Excepciones/KMeansExceptions.java
+++ src/main/java/es/uji/al405104/Excepciones/KMeansExceptions.java
@@ -2,4 +2,7 @@ package es.uji.al405104.Excepciones;
 
 public class KMeansExceptions extends Exception {
 
+    public  KMeansExceptions(String message) {
+        super(message); //Enviamos el texto de error a la clase Exception
+    }
 }
