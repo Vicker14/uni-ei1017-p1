ffc83515392ecfb24e451eaa4ab5caaf4fa61c6d
Alex Popa <alexpopa7473@gmail.com>
2026-03-25 14:44:06 +0100

    Añadido comentario a InvalidClusterNumberException

--- src/main/java/es/uji/al405104/Excepciones/InvalidClusterNumberException.java
+++ src/main/java/es/uji/al405104/Excepciones/InvalidClusterNumberException.java
@@ -2,6 +2,10 @@ package es.uji.al405104.Excepciones;
 
 public class InvalidClusterNumberException extends KMeansExceptions{
 
+    /*
+        Esta excepcion tiene que extender de KMeansException porque en la interfaz Algorithm
+        hemos puesto que el metodo train pueda lanzar errores solo de KMeansException
+     */
     public InvalidClusterNumberException(int k, int n) {
         super("Error: Has pedido " + k + " grupos poro solo hay " + n + " datos");
     }
