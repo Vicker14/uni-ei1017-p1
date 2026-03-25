8f0f3a63c460d0463c5b7659f3febfbae700b63a
Alex Popa <alexpopa7473@gmail.com>
2026-03-25 14:55:51 +0100

    Añadido los requisitos finales de InvalidClusterNumberException
    Añadido:
    
    -Atributos privados para guardar los datos (Num grupos, Total data)
    -Getters de los dos atributos

--- src/main/java/es/uji/al405104/Excepciones/InvalidClusterNumberException.java
+++ src/main/java/es/uji/al405104/Excepciones/InvalidClusterNumberException.java
@@ -6,7 +6,22 @@ public class InvalidClusterNumberException extends KMeansExceptions{
         Esta excepcion tiene que extender de KMeansException porque en la interfaz Algorithm
         hemos puesto que el metodo train pueda lanzar errores solo de KMeansException
      */
-    public InvalidClusterNumberException(int k, int n) {
-        super("Error: Has pedido " + k + " grupos poro solo hay " + n + " datos");
+
+    private int invalidClustrerNumber;
+    private int totalDataSize;
+
+    public InvalidClusterNumberException(int n, int k) {
+        super("Error: El numero de cluster es (" + n + ") no puede ser mayor que el total de datos (" + k + ")");
+
+        this.invalidClustrerNumber = n;
+        this.totalDataSize = k;
+    }
+
+    public int getInvalidClustrerNumber() {
+        return this.invalidClustrerNumber;
+    }
+
+    public int getTotalDataSize() {
+        return this.totalDataSize;
     }
 }
