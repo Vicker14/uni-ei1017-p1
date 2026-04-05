ba8882de1f4fe98dd07dcab45ddb0c39fa5584a6
vicker <vcalero2001@gmail.com>
2026-04-05 17:08:28 +0200

    Añadida una linea de explicación a la excepción base de KMeans

--- src/main/java/es/uji/al405104/Excepciones/KMeansExceptions.java
+++ src/main/java/es/uji/al405104/Excepciones/KMeansExceptions.java
@@ -2,6 +2,7 @@ package es.uji.al405104.Excepciones;
 
 public class KMeansExceptions extends Exception {
 
+    // Usado para cuando los datos proporcionados son nulo o no válidos
     public  KMeansExceptions(String message) {
         super(message); //Enviamos el texto de error a la clase Exception
     }
