303497103bbcb1d61245b09ed065f4c03c5712cb
vicker <vcalero2001@gmail.com>
2026-04-22 14:25:38 +0200

    Si ambas listas están vacías no retornará nada

--- src/main/java/es/uji/al405104/algorithms/distances/GenericDistance.java
+++ src/main/java/es/uji/al405104/algorithms/distances/GenericDistance.java
@@ -6,12 +6,15 @@ public abstract class GenericDistance {
 
     public static void comprobacionParametros(List<Double> p, List<Double> q) throws IllegalArgumentException {
 
-        if (p == null || p.isEmpty()) {
-            throw new IllegalArgumentException("El punto p no puede ser nulo o vacio");
+        if (p == null) {
+            throw new IllegalArgumentException("El punto p no puede ser nulo");
         }
-        if (q == null || q.isEmpty()) {
-            throw new IllegalArgumentException("El punto q no puede ser nulo o vacio");
+        if (q == null) {
+            throw new IllegalArgumentException("El punto q no puede ser nulo ");
         }
+
+        if (p.isEmpty() && q.isEmpty()) return;
+
         if (p.size() != q.size()) {
             throw new IllegalArgumentException("Los puntos deben tener el mismo tamaño");
         }
